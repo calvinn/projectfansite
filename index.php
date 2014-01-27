fe1383875a782e4c243b20616d63cb08a4e85744
calvinn <gezelligewebsite@live.nl>
2014-01-27 19:41:19 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+includ 'content/header.php';
+
+include 'content/menu';
+?>
+
+<div id='content'>
+<h1Welkom</h1><br>
+<b>Welkom Op Mijn Site!</b><br>
+Welkom op Mijn SIte we wensen je veel plezier!!!!!
+
+
+
+</div>
+
+
+(Dit is niet de echt index maar ne voorbeeld de css krijgen jullie niet.)
